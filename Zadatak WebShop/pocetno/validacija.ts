477fb47080ca3397ea3d7274f92d95e83a03766a
Milica Varsandan <milicamarjanovic89@gmail.com>
2019-07-30 23:11:49 +0200

    zadaci

---
+++ Zadatak WebShop/pocetno/validacija.ts
@@ -0,0 +1,9 @@
+function validiraj(forma: HTMLFormElement): boolean {
+	let ime: string = (forma.ime as HTMLInputElement).value;
+	let prezime: string = (forma.lozinka as HTMLInputElement).value;
+	if(ime == "pera" && prezime == "peric")
+		return true;
+	else if(ime == "mika" && prezime == "mikic")
+		return true;
+	return false;
+}
